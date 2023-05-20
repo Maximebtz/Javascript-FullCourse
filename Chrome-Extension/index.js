6028c12a82f6ec8c1a491793e7884997c29fdc08
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-05-20 23:21:27 +0200

    Update

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -1,6 +1,8 @@
 let myLeads = [];
+let oldLeads = []
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
 const clearBtn = document.getElementById("clear-btn");
 let ulEl = document.getElementById("ul-el");
 ulEl.style.display = "none";
@@ -11,40 +13,59 @@ console.log(leadsFromLocalStorage)
 
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads()
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-    renderLeads();
-
-    console.log(localStorage.getItem("myLeads"));
-});
-
-clearBtn.addEventListener("dblclick", function() {
-
-    localStorage.clear();
-    myLeads = []
-    renderLeads()
-
-});
-
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    myLeads = JSON.parse(localStorage.getItem("myLeads"));
+    leads = JSON.parse(localStorage.getItem("myLeads"));
     
-    if (myLeads === null || myLeads.length === 0) { // Vérification si myLeads est null ou vide
+    if (leads === null || leads.length === 0) { // Vérification si myLeads est null ou vide
         ulEl.style.display = "none";
     } else {
         ulEl.style.display = "block";
-        for (let i = 0; i < myLeads.length; i++) {
-            listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`;
+        for (let i = 0; i < leads.length; i++) {
+            listItems += `
+                        <li>
+                            <a target='_blank' href='${leads[i]}'>
+                                ${leads[i]}
+                            </a>
+                        </li>`;
         }
         ulEl.innerHTML = listItems;
     }
 }
+
+clearBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render();
+});
+
+
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value) {
+        if (myLeads === null) {
+            myLeads = [];
+        }
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+        render(myLeads);
+    }
+});
+
+
+
