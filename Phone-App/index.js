b792460f21255be0bb9528d2810accb46b4c5018
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-05-24 14:31:10 +0200

    Update

--- Phone-App/index.js
+++ Phone-App/index.js
@@ -1,9 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListeinDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    
+    Push(shoppingListeinDB, inputValue)
     
     console.log(inputValue)
 })
