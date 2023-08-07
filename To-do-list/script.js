3fa041c9283ccf1f7e6fde2c40c776b160a03143
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-08-07 15:36:20 +0200

    Update

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -2,31 +2,33 @@ const addButtonEl = document.getElementById("btn");
 const todoCard = document.querySelector('#todoCard');
 let taskCount = 1;
 
-addButtonEl.addEventListener("click", function() {
+    addButtonEl.addEventListener("click", function() {
 
-    // Cloner la première carte ou créer une nouvelle carte si c'est la première tâche
-    const clonedCard = taskCount === 1 ? todoCard : todoCard.cloneNode(true);
+        // Cloner la première carte ou créer une nouvelle carte si c'est la première tâche
+        const clonedCard = taskCount === 1 ? todoCard : todoCard.cloneNode(true);
 
-    // Modifier le texte du textarea dans la carte clonée
-    const textarea = clonedCard.querySelector('.task');
-    textarea.textContent = `Task ${taskCount}`;
+        // Modifier le texte du textarea dans la carte clonée
+        const textarea = clonedCard.querySelector('.task');
+        textarea.textContent = `Task ${taskCount}`;
 
-    // Ajouter la carte clonée au conteneur des cartes
-    const todoCardsContainer = document.getElementById("todoCards");
-    todoCardsContainer.appendChild(clonedCard);
+        // Ajouter la carte clonée au conteneur des cartes
+        const todoCardsContainer = document.getElementById("todoCards");
+        todoCardsContainer.appendChild(clonedCard);
 
 
-    // Ajouter la fonctionnalité de suppression à chaque bouton de suppression
-    const deleteButton = clonedCard.querySelector('.delBtn');
-    deleteButton.addEventListener('click', function() {
-        deleteCard(clonedCard);
+        // Ajouter la fonctionnalité de suppression à chaque bouton de suppression
+        const deleteButton = clonedCard.querySelector('.delBtn');
+        deleteButton.addEventListener('click', function() {
+            deleteCard(clonedCard);
+        });
+
+
+        // Augmenter le compteur de tâches
+        taskCount++;
     });
 
 
-    // Augmenter le compteur de tâches
-    taskCount++;
-});
-
+    
     // Fontion pour delete la card
     function deleteCard(card) {
         card.remove();
